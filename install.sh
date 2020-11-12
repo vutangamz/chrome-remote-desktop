8e3cc91b624a4e1328ab36f8def2dfb9b179d481
vutangamz <32426696+vutangamz@users.noreply.github.com>
2020-11-12 23:31:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 #
 # See environmental variables at then end of the script for configuration
 #
-
 sudo apt update
 sudo apt-get install --assume-yes wget
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -14,9 +13,6 @@ sudo DEBIAN_FRONTEND=noninteractive \
     apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-
-
 echo "Chrome remote desktop installation completed"
