399d86288bb68c95d35ac1fc88f6af3bbd85ee52
vutangamz <32426696+vutangamz@users.noreply.github.com>
2020-11-26 10:37:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
     apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
-sudo apt-get install xfce4-session
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
